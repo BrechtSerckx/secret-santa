96dd31a9dd0235b2b6715a7c50dfaa8bbe3e5a95
Brecht Serckx <brecht_serckx@hotmail.com>
2018-01-31 22:24:27 +0100

    example form

--- src/Handler/Santa.hs
+++ src/Handler/Santa.hs
@@ -7,10 +7,38 @@ module Handler.Santa where
 
 import Import
 
+
+data Car = Car
+    { carModel :: Text
+    , carYear  :: Int
+    } deriving Show
+
+carAForm :: AForm Handler Car
+carAForm = Car
+    <$> areq textField "Model" (Just "tesla")
+    <*> areq intField "Year" (Just 2015)
+
+carForm :: Html -> MForm Handler (FormResult Car, Widget)
+carForm = renderTable carAForm
+
+
 getSantaR :: Handler Html
 getSantaR = do
+    (formWidget, formEnctype) <- generateFormPost carForm
     defaultLayout $ do
         aDomId <- newIdent
         setTitle "Secret Santa - BrechtSerckx.be"
         $(widgetFile "santa")
 
+
+postSantaR :: Handler Html
+postSantaR = do
+    ((result, formWidget), formEnctype) <- runFormPost carForm
+    --let submission = case result of
+    --        FormSuccess res -> Just res
+    --        _ -> Nothing
+
+    defaultLayout $ do
+        aDomId <- newIdent
+        setTitle "Welcome To Yesod!"
+        $(widgetFile "santa")
