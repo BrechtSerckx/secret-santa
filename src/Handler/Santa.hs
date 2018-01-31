2f8a5f866f5692ac90d50f57bf0abfcc43840b82
Brecht Serckx <brecht_serckx@hotmail.com>
2018-02-01 00:40:46 +0100

    basic form working

--- src/Handler/Santa.hs
+++ src/Handler/Santa.hs
@@ -3,37 +3,62 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Handler.Santa where
 
 import Import
+import Yesod.Form.Bootstrap3
 
-
-data Car = Car
-    { carModel :: Text
-    , carYear  :: Int
+data ParticipantList = ParticipantList
+    { carCustom  :: [Text]
     } deriving Show
 
-carAForm :: AForm Handler Car
-carAForm = Car
-    <$> areq textField "Model" (Just "tesla")
-    <*> areq intField "Year" (Just 2015)
 
-carForm :: Html -> MForm Handler (FormResult Car, Widget)
-carForm = renderTable carAForm
+santaAForm :: AForm Handler ParticipantList
+santaAForm = ParticipantList
+    <$> areq santaField "Participants" Nothing
+
+
+santaForm :: Html -> MForm Handler (FormResult ParticipantList, Widget)
+santaForm = renderBootstrap3 bh santaAForm
+    where
+        bh = BootstrapHorizontalForm
+            { bflLabelOffset = ColLg 0
+            , bflLabelSize = ColLg 1
+            , bflInputOffset = ColSm 1
+            , bflInputSize = ColLg 1
+            }
+
+
+santaField :: Field Handler [Text]
+santaField = Field
+    { fieldParse = \rawVals _fileVals ->
+        return $ validateSantaField rawVals
+    , fieldView = \idAttr nameAttr otherAttrs eResult isReq ->
+        [whamlet|
+            <input id=test name=#{nameAttr} *{otherAttrs} type=text>
+            <input id=test name=#{nameAttr} *{otherAttrs} type=text>
+            <input id=test name=#{nameAttr} *{otherAttrs} type=text>
+        |]
+    , fieldEnctype = UrlEncoded
+    }
+
+validateSantaField :: [Text] -> Either (SomeMessage (HandlerSite Handler)) (Maybe [Text])
+validateSantaField ps@(_:_) = Right $ Just ps
+validateSantaField [] = Left "invalid"
 
 
 getSantaR :: Handler Html
 getSantaR = do
-    (formWidget, formEnctype) <- generateFormPost carForm
+    (formWidget, formEnctype) <- generateFormPost santaForm
     defaultLayout $ do
         aDomId <- newIdent
         setTitle "Secret Santa - BrechtSerckx.be"
         $(widgetFile "santa")
 
-
 postSantaR :: Handler Html
 postSantaR = do
-    ((result, formWidget), formEnctype) <- runFormPost carForm
+    ((_, formWidget), formEnctype) <- runFormPost santaForm
     --let submission = case result of
     --        FormSuccess res -> Just res
     --        _ -> Nothing
