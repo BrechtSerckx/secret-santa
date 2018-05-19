97abfc95ec50e9bc37fc7fe5c3f95b3bd0e3cbba
Brecht Serckx <brecht_serckx@hotmail.com>
2018-05-19 20:42:15 +0200

    updated stuff

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -1,19 +1,18 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoImplicitPrelude         #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE TemplateHaskell           #-}
+{-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE ViewPatterns              #-}
 {-# LANGUAGE ExistentialQuantification #-}
 
 module Foundation where
 
-import Import.NoFoundation
-import Mail                        (MailService)
-import Text.Hamlet                 (hamletFile)
-import Text.Jasmine                (minifym)
-import Yesod.Core.Types            (Logger)
-import Yesod.Default.Util          (addStaticContentExternal)
+import           Import.NoFoundation
+import           Text.Hamlet                 (hamletFile)
+import           Text.Jasmine                (minifym)
+import           Yesod.Core.Types            (Logger)
+import           Yesod.Default.Util          (addStaticContentExternal)
 import qualified Yesod.Core.Unsafe as Unsafe
 import qualified Data.CaseInsensitive as CI
 import qualified Data.Text.Encoding as TE
@@ -23,22 +22,21 @@ import qualified Data.Text.Encoding as TE
 -- starts running, such as database connections. Every handler will have
 -- access to the data present here.
 data App = App
-    { appSettings    :: AppSettings
-    , appStatic      :: Static -- ^ Settings for static file serving.
-    , appHttpManager :: Manager
-    , appLogger      :: Logger
-    , appMailService :: MailService
-    }
+        { appSettings    :: AppSettings
+        , appStatic      :: Static -- ^ Settings for static file serving.
+        , appHttpManager :: Manager
+        , appLogger      :: Logger
+        }
 
 data MenuItem = MenuItem
-    { menuItemLabel :: Text
-    , menuItemRoute :: Route App
-    , menuItemAccessCallback :: Bool
-    }
+        { menuItemLabel :: Text
+        , menuItemRoute :: Route App
+        , menuItemAccessCallback :: Bool
+        }
 
 data MenuTypes
-    = NavbarLeft MenuItem
-    | NavbarRight MenuItem
+        = NavbarLeft MenuItem
+        | NavbarRight MenuItem
 
 -- This is where we define all of the routes in our application. For a full
 -- explanation of the syntax, please see:
@@ -60,118 +58,118 @@ type Form x = Html -> MForm (HandlerT App IO) (FormResult x, Widget)
 -- Please see the documentation for the Yesod typeclass. There are a number
 -- of settings which can be configured by overriding methods here.
 instance Yesod App where
-    -- Controls the base of generated URLs. For more information on modifying,
-    -- see: https://github.com/yesodweb/yesod/wiki/Overriding-approot
-    approot = ApprootRequest $ \app req ->
-        case appRoot $ appSettings app of
-            Nothing -> getApprootText guessApproot app req
-            Just root -> root
+        -- Controls the base of generated URLs. For more information on modifying,
+        -- see: https://github.com/yesodweb/yesod/wiki/Overriding-approot
+        approot = ApprootRequest $ \app req ->
+                case appRoot $ appSettings app of
+                        Nothing   -> getApprootText guessApproot app req
+                        Just root -> root
 
-    -- Store session data on the client in encrypted cookies,
-    -- default session idle timeout is 120 minutes
-    makeSessionBackend _ = Just <$> defaultClientSessionBackend
-        120    -- timeout in minutes
-        "config/client_session_key.aes"
+        -- Store session data on the client in encrypted cookies,
+        -- default session idle timeout is 120 minutes
+        makeSessionBackend _ = Just <$> defaultClientSessionBackend
+                120    -- timeout in minutes
+                "config/client_session_key.aes"
 
-    -- Yesod Middleware allows you to run code before and after each handler function.
-    -- The defaultYesodMiddleware adds the response header "Vary: Accept, Accept-Language" and performs authorization checks.
-    -- Some users may also want to add the defaultCsrfMiddleware, which:
-    --   a) Sets a cookie with a CSRF token in it.
-    --   b) Validates that incoming write requests include that token in either a header or POST parameter.
-    -- To add it, chain it together with the defaultMiddleware: yesodMiddleware = defaultYesodMiddleware . defaultCsrfMiddleware
-    -- For details, see the CSRF documentation in the Yesod.Core.Handler module of the yesod-core package.
-    yesodMiddleware = defaultYesodMiddleware
+        -- Yesod Middleware allows you to run code before and after each handler function.
+        -- The defaultYesodMiddleware adds the response header "Vary: Accept, Accept-Language" and performs authorization checks.
+        -- Some users may also want to add the defaultCsrfMiddleware, which:
+        --   a) Sets a cookie with a CSRF token in it.
+        --   b) Validates that incoming write requests include that token in either a header or POST parameter.
+        -- To add it, chain it together with the defaultMiddleware: yesodMiddleware = defaultYesodMiddleware . defaultCsrfMiddleware
+        -- For details, see the CSRF documentation in the Yesod.Core.Handler module of the yesod-core package.
+        yesodMiddleware      = defaultYesodMiddleware
 
-    defaultLayout widget = do
-        master <- getYesod
-        mmsg <- getMessage
+        defaultLayout widget = do
+                master           <- getYesod
+                mmsg             <- getMessage
 
-        mcurrentRoute <- getCurrentRoute
+                mcurrentRoute    <- getCurrentRoute
 
-        -- Get the breadcrumbs, as defined in the YesodBreadcrumbs instance.
-        (title, parents) <- breadcrumbs
+                -- Get the breadcrumbs, as defined in the YesodBreadcrumbs instance.
+                (title, parents) <- breadcrumbs
 
-        -- Define the menu items of the header.
-        let menuItems =
-                [ NavbarLeft $ MenuItem
-                    { menuItemLabel = "Home"
-                    , menuItemRoute = HomeR
-                    , menuItemAccessCallback = True
-                    }
-                , NavbarLeft $ MenuItem
-                    { menuItemLabel = "Secret Santa"
-                    , menuItemRoute = SantaR
-                    , menuItemAccessCallback = True
-                    }
-                ]
+                -- Define the menu items of the header.
+                let menuItems =
+                        [ NavbarLeft $ MenuItem
+                                { menuItemLabel = "Home"
+                                , menuItemRoute = HomeR
+                                , menuItemAccessCallback = True
+                                }
+                        , NavbarLeft $ MenuItem
+                                { menuItemLabel = "Secret Santa"
+                                , menuItemRoute = SantaR
+                                , menuItemAccessCallback = True
+                                }
+                        ]
 
-        let navbarLeftMenuItems = [x | NavbarLeft x <- menuItems]
-        let navbarRightMenuItems = [x | NavbarRight x <- menuItems]
+                let navbarLeftMenuItems          = [x | NavbarLeft x <- menuItems]
+                let navbarRightMenuItems         = [x | NavbarRight x <- menuItems]
 
-        let navbarLeftFilteredMenuItems = [x | x <- navbarLeftMenuItems, menuItemAccessCallback x]
-        let navbarRightFilteredMenuItems = [x | x <- navbarRightMenuItems, menuItemAccessCallback x]
+                let navbarLeftFilteredMenuItems  = [x | x <- navbarLeftMenuItems, menuItemAccessCallback x]
+                let navbarRightFilteredMenuItems = [x | x <- navbarRightMenuItems, menuItemAccessCallback x]
 
-        -- We break up the default layout into two components:
-        -- default-layout is the contents of the body tag, and
-        -- default-layout-wrapper is the entire page. Since the final
-        -- value passed to hamletToRepHtml cannot be a widget, this allows
-        -- you to use normal widget features in default-layout.
+                -- We break up the default layout into two components:
+                -- default-layout is the contents of the body tag, and
+                -- default-layout-wrapper is the entire page. Since the final
+                -- value passed to hamletToRepHtml cannot be a widget, this allows
+                -- you to use normal widget features in default-layout.
 
-        pc <- widgetToPageContent $ do
-            addStylesheet $ StaticR css_bootstrap_css
-            $(widgetFile "default-layout")
-        withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
+                pc <- widgetToPageContent $ do
+                        addStylesheet $ StaticR css_bootstrap_css
+                        $(widgetFile "default-layout")
+                withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
-    -- Routes not requiring authenitcation.
-    isAuthorized FaviconR _ = return Authorized
-    isAuthorized RobotsR _ = return Authorized
-    -- Default to Authorized for now.
-    isAuthorized _ _ = return Authorized
+        -- Routes not requiring authenitcation.
+        isAuthorized FaviconR _ = return Authorized
+        isAuthorized RobotsR _  = return Authorized
+        -- Default to Authorized for now.
+        isAuthorized _ _        = return Authorized
 
-    -- This function creates static content files in the static folder
-    -- and names them based on a hash of their content. This allows
-    -- expiration dates to be set far in the future without worry of
-    -- users receiving stale content.
-    addStaticContent ext mime content = do
-        master <- getYesod
-        let staticDir = appStaticDir $ appSettings master
-        addStaticContentExternal
-            minifym
-            genFileName
-            staticDir
-            (StaticR . flip StaticRoute [])
-            ext
-            mime
-            content
-      where
-        -- Generate a unique filename based on the content itself
-        genFileName lbs = "autogen-" ++ base64md5 lbs
+        -- This function creates static content files in the static folder
+        -- and names them based on a hash of their content. This allows
+        -- expiration dates to be set far in the future without worry of
+        -- users receiving stale content.
+        addStaticContent ext mime content = do
+                master <- getYesod
+                let staticDir = appStaticDir $ appSettings master
+                addStaticContentExternal
+                        minifym
+                        genFileName
+                        staticDir
+                        (StaticR . flip StaticRoute [])
+                        ext
+                        mime
+                        content
+          where
+                -- Generate a unique filename based on the content itself
+                genFileName lbs = "autogen-" ++ base64md5 lbs
 
-    -- What messages should be logged. The following includes all messages when
-    -- in development, and warnings and errors in production.
-    shouldLog app _source level =
-        appShouldLogAll (appSettings app)
-            || level == LevelWarn
-            || level == LevelError
+        -- What messages should be logged. The following includes all messages when
+        -- in development, and warnings and errors in production.
+        shouldLog app _source level
+                =  appShouldLogAll (appSettings app)
+                || level == LevelWarn
+                || level == LevelError
 
-    makeLogger = return . appLogger
+        makeLogger = return . appLogger
 
 -- Define breadcrumbs.
 instance YesodBreadcrumbs App where
-  breadcrumb HomeR = return ("Home", Nothing)
+  breadcrumb HomeR  = return ("Home", Nothing)
   breadcrumb SantaR = return ("Home/Secret Santa", Nothing)
-  breadcrumb  _ = return ("home", Nothing)
+  breadcrumb  _     = return ("home", Nothing)
 
 -- This instance is required to use forms. You can modify renderMessage to
 -- achieve customized and internationalized form validation messages.
 instance RenderMessage App FormMessage where
-    renderMessage _ _ = defaultFormMessage
+        renderMessage _ _ = defaultFormMessage
 
 -- Useful when writing code that is re-usable outside of the Handler context.
 -- An example is background jobs that send email.
 -- This can also be useful for writing code that works across multiple Yesod applications.
 instance HasHttpManager App where
-    getHttpManager = appHttpManager
+        getHttpManager = appHttpManager
 
 unsafeHandler :: App -> Handler a -> IO a
 unsafeHandler = Unsafe.fakeHandlerGetLogger appLogger
--- src/Mail.hs
+++ src/Mail.hs
@@ -1,117 +1,107 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveDataTypeable		 #-}
-{-# LANGUAGE FlexibleContexts		   #-}
-{-# LANGUAGE GADTs					  #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses	  #-}
-{-# LANGUAGE OverloadedStrings		  #-}
-{-# LANGUAGE QuasiQuotes				#-}
-{-# LANGUAGE TemplateHaskell			#-}
-{-# LANGUAGE TypeFamilies			   #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 module Mail where
 
 import           Network.Mail.Mime  hiding (htmlPart)
-import           Network.Mail.Mime.SES (SES(..),renderSendMailSESGlobal)
+import qualified Network.Mail.Mime.SES as SES
 import           Network.Mail.SMTP (sendMailWithLogin')
 import           Network.Mail.SMTP.Auth (UserName, Password)
 import           Network.Socket (HostName, PortNumber)
 import           Text.Blaze.Html.Renderer.Utf8 (renderHtml)
 import           Text.Shakespeare.Text (stext)
 import           Text.Hamlet (shamlet)
-import           Data.ByteString (ByteString)
-import qualified Data.Text.Lazy.Encoding
 import           Data.Text
-
-
-from = Address (Just "Secret Santa" ) "secret-santa@secret-santa.net" 
-cc = []
-bcc = []
-subject = "Your Secret Santa Match"
-
-
-data MailService 
-	= SendMailService
-	| GMailService
-		{ gMailUsername :: UserName
-		, gMailPassword :: Password
-		}
-	| SesMailService
-		{ sesMailSes :: SES
-		}
-
-data MailServiceSettings 
-	= SendMailSettings
-	| GMailSettings
-		{ gMailSettingsUserName :: UserName
-		, gMailSettingsPassword :: Password
-		}
-	| SesMailSettings
-		{ sesMailSettingsFrom :: ByteString
-		, sesMailSettingsTo   :: [ByteString]
-		, sesAccessKey        :: ByteString
-		, sesSecretKey        :: ByteString
-		, sesSessionToken     :: Maybe ByteString
-		, sesRegion           :: Text
-		}
-
-	
-makeMailService :: MailServiceSettings -> MailService
-makeMailService SendMailSettings = SendMailService
-makeMailService (GMailSettings u p) = GMailService u p
-makeMailService (SesMailSettings from to aKey sKey token region)
-	=
-		let ses = SES from to aKey sKey token region
-		in  SesMailService ses
+import qualified Data.Text.Lazy.Encoding as Enc.Lazy (encodeUtf8)
+import qualified Data.Text.Encoding as Enc.Strict (encodeUtf8)
 
 
 
-sendMail :: MailService -> Mail -> IO ()
-sendMail SendMailService mail = renderSendMail mail
-sendMail (GMailService user pass) mail = sendMailWithLogin' host port user pass mail
-	where
-		host = "smtp.google.com" :: HostName
-		port_tls = 587 :: PortNumber
-		port_ssl = 465 :: PortNumber
-		port = port_tls
-sendMail (SesMailService ses) mail = renderSendMailSESGlobal ses mail
+data MailSettings 
+        = SendMailSettings
+                { mailOrigin      :: Address
+                , mailSubject     :: Text
+                }
+        | GMailSettings
+                { mailOrigin      :: Address
+                , mailSubject     :: Text
+                , gMailUserName   :: UserName
+                , gMailPassword   :: Password
+                }
+        | SesSettings
+                { mailOrigin      :: Address
+                , mailSubject     :: Text
+                , sesAccessKey    :: Text
+                , sesSecretKey    :: Text
+                , sesSessionToken :: Maybe Text
+                , sesRegion       :: Text
+                }
 
 
-mkMail :: Address -> Text -> Text -> Mail
-mkMail to participant match = (emptyMail $ from)
-		{ mailTo = [to]
-		, mailHeaders =
-			[ ("Subject", subject)
-			]
-		, mailParts = [[textPart, htmlPart]]
-		}
-	where
-		textPart = mkTextPart participant match
-		htmlPart = mkHtmlPart participant match
+sendMail :: MailSettings -> Mail -> IO ()
+sendMail (SendMailSettings _ _) mail = renderSendMail mail
+sendMail (GMailSettings _ _ user pass) mail = sendMailWithLogin' host port user pass mail
+        where
+                host = "smtp.google.com" :: HostName
+                port_tls = 587 :: PortNumber
+                port_ssl = 465 :: PortNumber
+                port = port_tls
+sendMail sesSettings@(SesSettings _ _ _ _ _ _) mail 
+        = (flip SES.renderSendMailSESGlobal) mail
+        $ SES.SES
+                { SES.sesFrom         = Enc.Strict.encodeUtf8 . addressEmail $ mailOrigin sesSettings
+                , SES.sesTo           = Prelude.map (Enc.Strict.encodeUtf8 . addressEmail) $ mailTo mail
+                , SES.sesAccessKey    = Enc.Strict.encodeUtf8 $ sesAccessKey sesSettings
+                , SES.sesSecretKey    = Enc.Strict.encodeUtf8 $ sesSecretKey sesSettings
+                , SES.sesSessionToken = fmap Enc.Strict.encodeUtf8 $ sesSessionToken sesSettings
+                , SES.sesRegion       = sesRegion sesSettings
+                }
+
+
+mkMail :: MailSettings -> Address -> Text -> Text -> Mail
+mkMail mailSettings to participant match 
+        = (emptyMail $ mailOrigin mailSettings)
+                { mailTo = [to]
+                , mailHeaders =
+                        [ ("Subject", mailSubject mailSettings)
+                        ]
+                , mailParts = [[textPart, htmlPart]]
+                }
+        where
+                textPart = mkTextPart participant match
+                htmlPart = mkHtmlPart participant match
 
 mkTextPart :: Text -> Text -> Part
 mkTextPart participant match = Part
-	{ partType = "text/plain; charset=utf-8"
-	, partEncoding = None
-	, partFilename = Nothing
-	, partContent = Data.Text.Lazy.Encoding.encodeUtf8
-		[stext|
-			Hi #{participant}
-			You are Secret Santa for: #{match}!
-		|]
-	, partHeaders = []
-	}
+        { partType = "text/plain; charset=utf-8"
+        , partEncoding = None
+        , partFilename = Nothing
+        , partContent = Enc.Lazy.encodeUtf8
+                [stext|
+                        Hi #{participant}
+                        You are Secret Santa for: #{match}!
+                |]
+        , partHeaders = []
+        }
 
 mkHtmlPart :: Text -> Text -> Part
 mkHtmlPart participant match = Part
-	{ partType = "text/html; charset=utf-8"
-	, partEncoding = None
-	, partFilename = Nothing
-	, partContent = renderHtml
-		[shamlet|
+        { partType = "text/html; charset=utf-8"
+        , partEncoding = None
+        , partFilename = Nothing
+        , partContent = renderHtml
+                [shamlet|
 <p>Hi #{participant}
 <p>You are Secret Santa for: <b>#{match}<\b>!
 |]
-	, partHeaders = []
-	}
+        , partHeaders = []
+        }
 
 
