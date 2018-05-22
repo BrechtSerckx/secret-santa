13b457cb175c72e20b6b570185b9bbbc3b5de903
Brecht Serckx <brecht_serckx@hotmail.com>
2018-05-22 13:38:48 +0200

    removed language extensions from secretsanta

--- src/SecretSanta.hs
+++ src/SecretSanta.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE QuasiQuotes                #-}
-{-# LANGUAGE TemplateHaskell            #-}
 module SecretSanta where
 
 import           Control.Monad (guard)
